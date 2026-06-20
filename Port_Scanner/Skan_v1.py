e0e7c9f0eeb8b5eee0c7befeb2640c99df32a9e8
Demian-Chern <demianchernyavskiy@gmail.com>
2026-06-20 13:21:58 +0300

    рабочий сканер

--- Port_Scanner/Skan_v1.py
+++ Port_Scanner/Skan_v1.py
@@ -1,5 +1,16 @@
 import socket
 import sys
+import logging
+import argparse
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('Port_Scanner.log', encoding='utf-8'),
+        logging.StreamHandler()
+    ]
+)
 
 def scan_port(host, port, timeout= 0.5):
     # подключение TCP-порта на хост
@@ -26,19 +37,31 @@ def scan_port(host, port, timeout= 0.5):
 def self_format_result(port, status):
     return f'[+] порт {port:<5} | статус {status}'
 
+
 if __name__ == '__main__':
-        TARGET_HOST = input()
-        # TARGET_PORT = 80
-        TIMEOUT = 1
+    parser = argparse.ArgumentParser(description="Port Scanner v1.1")
+    parser.add_argument('--host', required=True, type=str, help='Целевой хост')
+    parser.add_argument('--ports', type=str, default='21,22,80,443', help='Порты через запятую')
+    parser.add_argument('--timeout', type=float, default=1.0, help='Таймаут')
 
-        print(f'запуск {TARGET_HOST}...')
-        # scan_port(TARGET_HOST, TARGET_PORT)
+    args = parser.parse_args()
 
-        #PORTS = [21, 22, 80, 443, 8080]
+    logging.info(f"Запуск сканирования хоста: {args.host}")
+    logging.info(f"Установленный таймаут: {args.timeout} сек.")
 
-        for port in range(1, 80):
-            scan_port(TARGET_HOST, port, TIMEOUT)
-
+    # Проверка на обман провайдера (Fake Open Check)
+    logging.info("[*] Проверка надежности сети...")
+    # Проверяем порт 9999, который у Google точно закрыт
+    if scan_port(args.host, 9999, timeout=args.timeout):
+        logging.warning("[!] ВНИМАНИЕ: Сеть или провайдер возвращают ложные ответы (блок-заглушки)!")
+        logging.warning("[!] Результаты сканирования внешних сайтов могут быть неточными.")
         print("-" * 50)
-        print("[*] Сканирование завершено.")
 
+    # Парсим порты
+    port_list = [int(p) for p in args.ports.split(',')]
+
+    # Запуск основного сканирования
+    for port in port_list:
+        scan_port(args.host, port, args.timeout)
+
+    logging.info("Сканирование успешно завершено. Результаты сохранены в scanner.log")
